622aaa9160627412fea14415b25425b8d244ed72
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2015-02-15 21:19:36 +0100

    Lesson 2

--- app/src/main/java/com/example/ramonh/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/ramonh/sunshine/app/MainActivity.java
@@ -1,19 +1,23 @@
 package com.example.ramonh.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.lang.reflect.Array;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 
@@ -25,7 +29,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -53,43 +57,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            ArrayList<String> strings = new ArrayList<String>();
-            strings.add("Today-Sunny-88/63");
-            strings.add("Today-Sunny-88/63");
-            strings.add("Today-Sunny-81/63");
-            strings.add("Today-Sunny-82/63");
-            strings.add("Today-Sunny-83/63");
-            strings.add("Today-Sunny-84/63");
-            strings.add("Today-Sunny-85/63");
-            strings.add("Today-Sunny-86/63");
-            strings.add("Today-Sunny-81/63");
-            strings.add("Today-Sunny-82/63");
-            strings.add("Today-Sunny-83/63");
-            strings.add("Today-Sunny-84/63");
-            strings.add("Today-Sunny-85/63");
-            strings.add("Today-Sunny-86/63");
-
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                    getActivity(),
-                    R.layout.list_item_forecast,
-                    R.id.list_item_forecast_textview,
-                    strings);
-
-            ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(adapter);
-            return rootView;
-        }
-    }
 }
