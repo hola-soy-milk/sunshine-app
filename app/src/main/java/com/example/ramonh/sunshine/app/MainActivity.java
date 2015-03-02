64103a97af24b89adbf44aad38a9e192493ed6e0
ramon huidobro <ramon.martin.huidobro@gmail.com>
2015-03-02 13:42:13 +0100

    Finish lesson 4

--- app/src/main/java/com/example/ramonh/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/ramonh/sunshine/app/MainActivity.java
@@ -16,25 +16,30 @@ public class MainActivity extends ActionBarActivity {
 
     private final String LOG_TAG = "MainActivity";
 
+    private final String FORECASTFRAGMENT_TAG = "FFTAG";
 
+    private String mLocation;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mLocation = Utility.getPreferredLocation(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment())
+                    .add(R.id.container, new ForecastFragment(), FORECASTFRAGMENT_TAG)
                     .commit();
         }
     }
+
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.d(LOG_TAG, "Started");
     }
+
 
     @Override
     protected void onStop() {
@@ -52,6 +57,16 @@ public class MainActivity extends ActionBarActivity {
     protected void onResume() {
         super.onResume();
         Log.d(LOG_TAG, "Resumed");
+
+        String location = Utility.getPreferredLocation(this);
+
+        if(location != null && !location.equals(mLocation)){
+            ForecastFragment ff = (ForecastFragment) getSupportFragmentManager().findFragmentByTag(FORECASTFRAGMENT_TAG);
+            if (null != ff) {
+                ff.onLocationChanged();
+            }
+            mLocation = location;
+        }
     }
 
     @Override
